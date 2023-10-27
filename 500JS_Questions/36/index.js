1f9bce85261b270a8e2c9c77cc84b707b25cf560
ravi-kumar5-kiwitech <ravi.kumar5@kiwitech.com>
2023-10-27 16:52:21 +0530

    learning js

--- 500JS_Questions/36/index.js
+++ 500JS_Questions/36/index.js
@@ -127,7 +127,295 @@ console.log(charsFromASCII);//ABC
 /*
     An ArrayBuffer object is used to represent a generic, fixed-length raw binary data buffer. 
     You can create it as below,
+    e.g.
+    var buff
+    let buffer = new ArrayBuffer(16); // create a buffer of length 16
+    alert(buffer.byteLength); // 16
+
+    *To manipulate an ArrayBuffer, we need to use a “view” object.
+    *Create a DataView referring to the buffer
+    let view = new DataView(buffer);
 */
-var buff
-let buffer = new ArrayBuffer(16); // create a buffer of length 16
-alert(buffer.byteLength); // 16
+
+//What is the purpose of Error object
+/*
+    The Error constructor creates an error object and the instances of error objects are thrown 
+    when runtime errors occur. 
+    The Error object can also be used as a base object for user-defined exceptions. 
+    The syntax of error object would be as below,
+
+    new Error([message[, fileName[, lineNumber]]])
+*/
+//You can throw user defined exceptions or errors using Error object in try...catch block as below,
+try {
+    let balance = 1000;
+    let withdraw = 2000
+    if(withdraw > balance){
+        throw new Error('Oops, You don"t have enough money!');
+    }
+} catch (error) {
+    console.log("Error: ",error.message);//Error:  Oops, You don"t have enough money!
+}
+
+//What is the purpose of EvalError object
+/*
+    The EvalError object indicates an error regarding the global eval() function. 
+    Even though this exception is not thrown by JavaScript anymore, the EvalError object remains 
+    for compatibility. 
+    The syntax of this expression would be as below,
+
+    new EvalError([message[, fileName[, lineNumber]]])
+*/
+
+//You can throw EvalError with in try...catch block as below,
+try {
+    throw new EvalError('Eval function error', 'someFile.js', 100);
+} catch (e) {
+    console.log(e.message, e.name, e.fileName);// "Eval function error", "EvalError", "someFile.js"
+}
+
+//Do all objects have prototypes
+/*
+    No. All objects have prototypes except for the base object which is created by the user, or 
+    an object that is created using the new keyword.
+*/
+
+//What is the difference between a parameter and an argument
+/*
+    Parameter is the variable name of a function definition whereas an argument represents the 
+    value given to a function when it is invoked. 
+    Let's explain this with a simple function
+*/
+
+function person(parameter1,parameter2,parameter3){
+    console.log(parameter1,parameter2,parameter3);
+    return {parameter1,parameter2,parameter3}
+}
+const [arg1,agr2,agr3] = ['ravi',25,'ravi@getnada.com'];
+person(arg1,agr2,agr3);
+
+
+//What is the purpose of some method in arrays
+/*
+    The some() method is used to test whether at least one element in the array passes the test 
+    implemented by the provided function. 
+    The method returns a boolean value. 
+    Let's take an example to test for any odd elements,
+*/
+
+var arr = [1,2,3,4,5,6,7,8,9];
+var someOdd = arr.some(item => item%2!=0);
+console.log("someOdd <><>",someOdd);// true
+
+//How do you combine two or more arrays
+/*
+    The concat() method is used to join two or more arrays by returning a new array containing 
+    all the elements. 
+    The syntax would be as below,
+    let arr = [];
+    arr.contact(arr1,arr2,arr3,.......arr);
+*/
+//Let's take an example of array's concatenation with veggies and fruits arrays,
+var veggies = ["Tomato", "Carrot", "Cabbage"];
+var fruits = ["Apple", "Orange", "Pears"];
+var veggiesAndFruits = veggies.concat(fruits);
+console.log(veggiesAndFruits); // Tomato, Carrot, Cabbage, Apple, Orange, Pears
+
+
+
+//What is the difference between Shallow and Deep copy
+/*
+    There are two ways to copy an object,
+*/
+
+/*
+    Shallow Copy: 
+    Shallow copy is a bitwise copy of an object. 
+    A new object is created that has an exact copy of the values in the original object. 
+    If any of the fields of the object are references to other objects, just the reference 
+    addresses are copied i.e., 
+    only the memory address is copied.
+*/
+var empDetails = {
+    name: "John",
+    age: 25,
+    expertise: "Software Developer",
+};
+// to create a duplicate
+
+var empDetailsShallowCopy = empDetails; //Shallow copying!
+//if we change some property value in the duplicate one like this:
+
+empDetailsShallowCopy.name = "Johnson";
+console.log("Original empDetails Object: ",empDetails);
+console.log("Duplicate empDetails Object: ",empDetailsShallowCopy);
+/*
+    The above statement will also change the name of empDetails, since we have a shallow copy. 
+    That means we're losing the original data as well.
+*/
+
+
+
+/*
+    Deep copy: A deep copy copies all fields, and makes copies of dynamically allocated memory 
+    pointed to by the fields. 
+    A deep copy occurs when an object is copied along with the objects to which it refers.
+*/
+var empDetails = {
+    name: "John",
+    age: 25,
+    expertise: "Software Developer",
+};
+
+//Create a deep copy by using the properties from the original object into new variable
+var empDetailsDeepCopy = {
+    name: empDetails.name,
+    age: empDetails.age,
+    expertise: empDetails.expertise,
+};
+//Now if you change empDetailsDeepCopy.name, it will only affect empDetailsDeepCopy & not empDetails
+
+/*
+    Deep cloning in JavaScript involves creating a completely independent copy of an object or 
+    data structure, including all nested objects and their properties. 
+    There are various ways to achieve deep cloning in JavaScript. 
+    Here are some common methods:
+*/
+
+//Using JSON.parse and JSON.stringify:
+/*
+    This is a straightforward and widely used method for deep cloning objects. 
+    It works well for simple data structures but has limitations, as it doesn't support all 
+    object types, such as functions.
+*/
+function deepClone(obj) {
+    return JSON.parse(JSON.stringify(obj));
+}
+var originalObj = { a: 1, b: { c: 2 } };
+var clonedObj = deepClone(originalObj);
+
+//Using a Recursive Function:
+/*
+    You can create a recursive function to traverse the object and its nested properties, 
+    creating new objects for each level of nesting.
+*/
+function deepClone(obj) {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+    const clone = Array.isArray(obj) ? [] : {};
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            clone[key] = deepClone(obj[key]);
+        }
+    }
+    return clone;
+}
+
+var originalObj = { a: 1, b: { c: 2 } };
+var clonedObj = deepClone(originalObj);
+
+//Using External Libraries:
+/*
+    There are external libraries like Lodash that provide a cloneDeep function for deep cloning.
+
+    const _ = require('lodash');
+    const originalObj = { a: 1, b: { c: 2 } };
+    const clonedObj = _.cloneDeep(originalObj);
+*/
+
+//Custom Deep Clone Function:
+/*
+    You can create a custom deep clone function tailored to your specific needs. 
+    This allows you to handle edge cases or custom behavior when cloning objects.
+*/
+function customDeepClone(obj) {
+    // Your custom cloning logic here
+}
+
+var originalObj = { a: 1, b: { c: 2 } };
+var clonedObj = customDeepClone(originalObj);
+
+
+/*
+    Each of these methods has its advantages and limitations, so choose the one that best fits 
+    your use case and requirements. When implementing deep cloning, be cautious about circular 
+    references and consider whether certain object types or properties should be handled 
+    differently during cloning.
+*/
+
+//How do you create specific number of copies of a string
+/*
+    The repeat() method is used to construct and return a new string which contains the specified 
+    number of copies of the string on which it was called, concatenated together. 
+    Remember that this method has been added to the ECMAScript 2015 specification. 
+    Let's take an example of Hello string to repeat it 4 times,
+*/
+
+var str = "Hello";
+var re = str.repeat(4);
+console.log(re);//HelloHelloHelloHello
+
+//How do you trim a string at the beginning or ending
+/*
+    The 'trim' method of string prototype is used to trim on both sides of a string. 
+    But if you want to trim especially at the beginning or ending of the string then you can use 
+    'trimStart'/'trimLeft' and 'trimEnd'/'trimRight' methods. 
+    Let's see an example of these methods on a greeting message,
+*/
+
+var greeting = "   Hello, Goodmorning!   ";
+
+console.log(greeting); // "   Hello, Goodmorning!   "
+console.log(greeting.trimStart()); // "Hello, Goodmorning!   "
+console.log(greeting.trimLeft()); // "Hello, Goodmorning!   "
+
+console.log(greeting.trimEnd()); // "   Hello, Goodmorning!"
+console.log(greeting.trimRight()); // "   Hello, Goodmorning!"
+
+//What is the output of below console statement with unary operator
+console.log(+"Hello");// NaN
+/*
+    The output of the above console log statement returns NaN. Because the element is prefixed 
+    by the unary operator and the JavaScript interpreter will try to convert that element into a 
+    number type. Since the conversion fails, the value of the statement results in NaN value.
+*/
+
+//Does javascript uses mixins
+/*
+    Mixin is a generic object-oriented programming term - is a class containing methods that can 
+    be used by other classes without a need to inherit from it. In JavaScript we can only inherit 
+    from a single object. ie. There can be only one [[prototype]] for an object.
+
+    But sometimes we require to extend more than one, to overcome this we can use Mixin which 
+    helps to copy methods to the prototype of another class.
+
+    Say for instance, we've two classes User and CleanRoom. Suppose we need to add CleanRoom 
+    functionality to User, so that user can clean the room at demand. Here's where concept called 
+    mixins comes into picture.
+*/
+
+// mixin
+let cleanRoomMixin = {
+    cleanRoom() {
+      alert(`Hello ${this.name}, your room is clean now`);
+    },
+    sayBye() {
+      alert(`Bye ${this.name}`);
+    },
+};
+  
+// usage:
+class User {
+    constructor(name) {
+      this.name = name;
+    }
+}
+
+// copy the methods
+Object.assign(User.prototype, cleanRoomMixin);
+
+// now User can clean the room
+new User("Dude").cleanRoom(); // Hello Dude, your room is clean now!
+
+
