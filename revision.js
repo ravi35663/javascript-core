ad19b820682e9a2e833ed3fe5a54dabade081165
ravi <ravikr845430@gmail.com>
2025-01-21 21:08:54 +0530

    learning react

--- revision.js
+++ revision.js
@@ -1,9 +1,7 @@
 /*
 ==> Revision Cycle:
     1)  === vs ==    =>  Done
-    2)  About-js     =>  Done
-    3)  Array        =>  Done
-    4)  Basic JS     =>  Done   
+    2)  About-js     =>  Done 
 */
 
 
